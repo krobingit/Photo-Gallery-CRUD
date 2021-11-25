47197a4bbe5f1e1e1d6f26e5da0cca240bfdea42
krobingit <liorobin5@gmail.com>
2021-11-26 03:50:23 +0530

    Changes

--- src/AddUser.js
+++ src/AddUser.js
@@ -3,6 +3,8 @@ import Button from '@mui/material/Button';
 import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { useFormik } from "formik";
+import { useState, useEffect } from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
 import * as yup from 'yup';
 export const formValidationSchema = ()=> yup.object({
   name: yup.string().required("Required Field"),
@@ -42,9 +44,21 @@ about:""},
       fontSize: '1.5rem', margin: "0"
     }
   };
+//loading animation
+ let [Loading, setLoading] = useState(false);
+ useEffect(() => {
+  setLoading(true);
+  setTimeout(() => {
+   setLoading(false)
+
+  }, 500)
+ }, [])
+
     return (
 
       <form onSubmit={handleSubmit} className="addUser-Form">
+        {Loading ? <ClipLoader color="D0021B" loading={Loading} size={75} /> :
+    <>
         <h1 className="TITLE"><PersonAddAltIcon style={{ fontSize: "4rem" }} /> Create User Here..</h1>
         <TextField style ={{width: '70%'}} FormHelperTextProps={helperTextStyle} inputProps={styles} InputLabelProps={styles} error={errors.name && touched.name} helperText={errors.name && touched.name && errors.name} variant="standard" value={values.name} id="name" name="name" onChange={handleChange} onBlur={handleBlur} label="Name" />
         <TextField style ={{width: '70%'}} FormHelperTextProps={helperTextStyle} inputProps={styles} InputLabelProps={styles} error={errors.displaypic && touched.displaypic} helperText={errors.displaypic && touched.displaypic && errors.displaypic}  variant="standard" value={values.displaypic} id="displaypic" name="displaypic" onBlur={handleBlur} onChange={handleChange} label="Display Picture URL" />
@@ -54,7 +68,9 @@ about:""},
         <TextField style={{ width: '70%' }} FormHelperTextProps={helperTextStyle} inputProps={styles} InputLabelProps={styles} error={errors.address && touched.address} helperText={errors.address && touched.address && errors.address} variant="standard" value={values.address} id="address" name="address" onBlur={handleBlur} onChange={handleChange} label="Address" />
         <TextField style={{ width: '70%' }} FormHelperTextProps={helperTextStyle} inputProps={styles} InputLabelProps={styles} error={errors.country && touched.country} helperText={errors.country && touched.country && errors.country} variant="standard" value={values.country} id="country" name="country" onBlur={handleBlur} onChange={handleChange} label="Country" />
         <TextField style ={{width: '70%'}} FormHelperTextProps={helperTextStyle} inputProps={styles} InputLabelProps={styles} error={errors.phone && touched.phone} helperText={errors.phone && touched.phone && errors.phone} variant="standard" value={values.phone} id="phone" name="phone" onBlur={handleBlur} onChange={handleChange} label="Phone Number" />
-        <Button type="submit" style={btnstyles} variant="contained">Add User</Button>
+            <Button type="submit" style={btnstyles} variant="contained">Add User</Button>
+             </>
+   }
       </form>
     );
 
